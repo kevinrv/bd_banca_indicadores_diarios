b1f4e893c042356af0e4de8d64ceaf45842afcc1
Kevin Rivera <kevinriveravergaray@gmail.com>
2025-05-20 21:05:20 -0500

    update

--- create_database/create_database.sql
+++ create_database/create_database.sql
@@ -1,4 +1,4 @@
--- Creación de la base de datos
+-- Creaciï¿½n de la base de datos
 
 CREATE DATABASE bd_banca_indicadores_diarios;
 GO
@@ -6,7 +6,7 @@ GO
 USE bd_banca_indicadores_diarios;
 GO
 /**/
--- Creación de la tabl "sucursales"
+-- Creaciï¿½n de la tabl "sucursales"
 CREATE TABLE sucursales (
 id INT IDENTITY(1,1) PRIMARY KEY,
 codigo VARCHAR(10) UNIQUE NOT NULL,
@@ -18,7 +18,7 @@ estado VARCHAR(25) NOT NULL,
 telefono VARCHAR(25) NULL
 );
 
--- Creación de la tabl "horas"
+-- Creaciï¿½n de la tabl "horas"
 CREATE TABLE horas (
 id INT IDENTITY(1,1) PRIMARY KEY,
 dia VARCHAR(25) NOT NULL,
@@ -26,7 +26,7 @@ hora_inicio TIME NOT NULL,
 hora_fin TIME NOT NULL
 );
 
--- Creación de la tabl "responsables"
+-- Creaciï¿½n de la tabl "responsables"
 
 CREATE TABLE responsables (
 id INT IDENTITY(1,1) PRIMARY KEY,
@@ -38,7 +38,7 @@ fecha_fin DATE NULL
 );
 
 
--- Creación de la tabl "sistemas_fuente"
+-- Creaciï¿½n de la tabl "sistemas_fuente"
 CREATE TABLE sistemas_fuente (
 id INT IDENTITY(1,1) PRIMARY KEY,
 responsable_id INT,
@@ -49,10 +49,10 @@ area Varchar(100) NOT NULL,
 CONSTRAINT fk_sistema_fuente_responsable FOREIGN KEY (responsable_id) REFERENCES responsables(id)
 );
 
-EXEC SP_HELP sistemas_fuente;
+--EXEC SP_HELP sistemas_fuente;
 
 
--- Creación de la tabla "horarios"
+-- Creaciï¿½n de la tabla "horarios"
 
 CREATE TABLE horarios(
 id INT IDENTITY(1,1) PRIMARY KEY,
@@ -66,10 +66,10 @@ FOREIGN KEY (sucursal_id) REFERENCES sucursales(id)
 ALTER TABLE horarios
 ADD FOREIGN KEY (hora_id) REFERENCES horas(id);
 
-EXEC SP_HELP horarios;
-SELECT GETDATE();
+--EXEC SP_HELP horarios;
+--SELECT GETDATE();
 
---- Creación de la tabla "Indicadores"
+--- Creaciï¿½n de la tabla "Indicadores"
 
 CREATE TABLE indicadores (
 id INT IDENTITY(1,1) PRIMARY KEY,
@@ -84,13 +84,39 @@ FOREIGN KEY (sistema_fuente_id) REFERENCES sistemas_fuente(id)
 );
 
 --- Crear tabla registros_diarios_indicadores
-CREATE TABLE registros_diarios_indicadores (id INT IDENTITY(1,1) PRIMARY KEY,sucursal_id INT NOT NULL,indicador_id INT NOT NULL,valor_meta DECIMAL(9,2),valor_real DECIMAL(9,2) NOT NULL,fecha_reporte DATETIME NOT NULL,FOREIGN KEY (sucursal_id) REFERENCES sucursales(id),FOREIGN KEY (indicador_id) REFERENCES indicadores(id));
+CREATE TABLE registros_diarios_indicadores (
+id INT IDENTITY(1,1) PRIMARY KEY,
+sucursal_id INT NOT NULL,
+indicador_id INT NOT NULL,
+valor_meta DECIMAL(9,2),
+valor_real DECIMAL(9,2) NOT NULL,
+fecha_reporte DATETIME NOT NULL,
+FOREIGN KEY (sucursal_id) REFERENCES sucursales(id),
+FOREIGN KEY (indicador_id) REFERENCES indicadores(id)
+);
 
 --- CREAR TABLA indicadores_horario
 
-CREATE TABLE indicadores_horario (id INT IDENTITY(1,1) PRIMARY KEY,horario_inicio_id INT NOT NULL,horario_fin_id INT NOT NULL,indicador_id INT NOT NULL,fecha_reporte DATETIME DEFAULT GETDATE() NOT NULL,valor DECIMAL(9,2) NOT NULL,FOREIGN KEY (horario_inicio_id) REFERENCES horarios(id),FOREIGN KEY (horario_fin_id) REFERENCES horarios(id),FOREIGN KEY (indicador_id) REFERENCES indicadores(id));
+CREATE TABLE indicadores_horario (
+id INT IDENTITY(1,1) PRIMARY KEY,
+horario_inicio_id INT NOT NULL,
+horario_fin_id INT NOT NULL,
+indicador_id INT NOT NULL,
+fecha_reporte DATETIME DEFAULT GETDATE() NOT NULL,
+valor DECIMAL(9,2) NOT NULL,
+FOREIGN KEY (horario_inicio_id) REFERENCES horarios(id),
+FOREIGN KEY (horario_fin_id) REFERENCES horarios(id),
+FOREIGN KEY (indicador_id) REFERENCES indicadores(id)
+);
 
 
 ---- Crear tabla desviaciones_indicador
 
-CREATE TABLE desviaciones_indicador (id INT IDENTITY(1,1) PRIMARY KEY,registro_diario_indicador_id INT NOT NULL,diferencia_absoluta DECIMAL(9,2)  NOT NULL,diferencia_porcentual DECIMAL(9,2) NOT NULL,clasificacion VARCHAR (100) NOT NULL,FOREIGN KEY (registro_diario_indicador_id) REFERENCES registros_diarios_indicadores(id));
+CREATE TABLE desviaciones_indicador (
+id INT IDENTITY(1,1) PRIMARY KEY,
+registro_diario_indicador_id INT NOT NULL,
+diferencia_absoluta DECIMAL(9,2)  NOT NULL,
+diferencia_porcentual DECIMAL(9,2) NOT NULL,
+clasificacion VARCHAR (100) NOT NULL,
+FOREIGN KEY (registro_diario_indicador_id) REFERENCES registros_diarios_indicadores(id)
+);
